d8a340ab9591ef8fd1340a906feb9379740eedef
ston3o <contact@ston3o.me>
2017-05-07 15:03:29 +0200

    Add twitter support

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Run
 ---
 
 ```
+# CREATE TABLE followers ( count INT, social_network VARCHAR(80), date DATETIME DEFAULT CURRENT_TIMESTAMP);
 # Edit .env config file
 make run
 ```
--- index.js
+++ index.js
@@ -2,20 +2,29 @@ import 'babel-polyfill';
 import mysql from 'mysql';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import Horseman from 'node-horseman';
 
 dotenv.config({ silent: true });
 
 (async () => {
-    const pinterestFollowers = async (username) => {
-        const res = await fetch(`https://api.pinterest.com/v3/pidgets/users/${username}/pins/`);
+    const pinterestFollowers = async (user) => {
+        const res = await fetch(`https://api.pinterest.com/v3/pidgets/users/${user}/pins/`);
         const json = await res.json();
         return json.data.user.follower_count;
     };
 
-    const facebookFollowers = async (username, token) => {
-        const res = await fetch(`https://graph.facebook.com/${username}?access_token=${token}&fields=likes`);
+    const facebookFollowers = async (user, token) => {
+        const res = await fetch(`https://graph.facebook.com/${user}?access_token=${token}&fields=likes`);
         const json = await res.json();
         return json.likes;
+    };
+
+    const twitterFollowers = async (user) => {
+        const followers = await new Horseman().open(`http://twitter.com/${user}`)
+			.text('.ProfileNav-item--followers .ProfileNav-value')
+			.close();
+
+        return followers;
     };
 
     const connection = mysql.createConnection(process.env.MYSQL_CONNECTION);
@@ -23,6 +32,7 @@ dotenv.config({ silent: true });
 
     connection.query('INSERT INTO followers SET ?', { count: await facebookFollowers(process.env.FACEBOOK_USER, process.env.TOKEN_FACEBOOK), social_network: 'facebook' });
     connection.query('INSERT INTO followers SET ?', { count: await pinterestFollowers(process.env.PINTEREST_USER), social_network: 'pinterest' });
+    connection.query('INSERT INTO followers SET ?', { count: await twitterFollowers(process.env.TWITTER_USER), social_network: 'twitter' });
 
     connection.end();
 })();
